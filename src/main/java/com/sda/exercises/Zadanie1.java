d38d09d6f969e744118911469728e7449a8fded0
Bartek Adamczuk <adamczuk.bartek1997@gmail.com>
2019-06-16 14:39:00 +0200

    zmiana galenzi i stworzenie klasy

--- src/main/java/com/sda/exercises/Zadanie1.java
+++ src/main/java/com/sda/exercises/Zadanie1.java
@@ -3,6 +3,7 @@ package com.sda.exercises;
 public class Zadanie1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!!!");
+        Powitanie powitanie = new Powitanie();
+        System.out.println(powitanie.helloWorld());
     }
 }
